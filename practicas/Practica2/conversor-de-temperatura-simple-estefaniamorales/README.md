a17a54c907bf48085af114c4f9201bdc2fac2f7f
alu0100698688 <alu0100698688@ull.edu.es>
2016-02-25 05:10:43 +0000

    Añadida práctica 2 a la web de prácticas

---
+++ practicas/Practica2/conversor-de-temperatura-simple-estefaniamorales/README.md
@@ -0,0 +1,17 @@
+# ETSII ULL Grado de Informática
+
+## Práctica: Conversor en JavaScript de grados Celsius a Farenheit y viceversa
+
+### Que aprendemos
+
+* HTML básico
+* JavaScript básico
+* Expresiones Regulares
+* Un poquito de CSS
+* Uso de Emmet (instale el plugin para su editor)
+
+### Enlace página donde se alojan las prácticas de la asignatura
+
+* [Web de las prácticas](http://alu0100698688.github.io/web/)
+
+### Enlace a la práctica
---
+++ practicas/Practica2/conversor-de-temperatura-simple-estefaniamorales/temperature.js
@@ -0,0 +1,27 @@
+"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+function calculate() {
+  var result;
+  var temp = original.value;
+  var regexp = /([\s]?[-+]?\d+(\.\d+)?(e[-+]?\d+)?)\s*(f$|fa$|far$|fare$|faren$|farenh$|farenhe$|farenhei$|farenheit$|c$|ce$|cel$|cels$|celsi$|celsiu$|celsius$)/i;
+
+  var m = temp.match(regexp);
+
+  if (m) {
+    var num = m[1];
+    var type = m[4];
+    num = parseFloat(num);
+    if (type[0] == 'c' || type[0] == 'C') {
+        result = (num * 9/5)+32;
+        result = result.toFixed(1)+" Farenheit"
+    }
+    else {
+        result = (num - 32)*5/9;
+        result = result.toFixed(1)+" Celsius"
+    }
+
+    converted.innerHTML = result;
+  }
+  else {
+    converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+  }
+}
